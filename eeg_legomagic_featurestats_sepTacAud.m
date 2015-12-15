1ca52cdcaac9eb4cbed62c08c52062599486e2cc
Johanna M Zumer <zumerj@psychl-132432-1.bham.ac.uk>
2015-12-15 09:45:17 +0000

    rm init date

--- eeg_legomagic_featurestats_sepTacAud.m
+++ eeg_legomagic_featurestats_sepTacAud.m
@@ -23,40 +23,40 @@ else
 end
 cd(edir)
 
-sub{100}='p01'; % ma.a. 03/04/14
-sub{1}='e01'; % ab.m. 21/05/14
-sub{2}='e02'; % ma.a. 04/06/14
-sub{3}='e03'; % ag.m. 10/06/14
-sub{4}='e04'; % re.g. 17/06/14
+% sub{100}='p01'; 
+sub{1}='e01'; 
+sub{2}='e02'; 
+sub{3}='e03'; 
+sub{4}='e04'; 
 %%%  above is pilot, below is real
-sub{5}='e05'; % ma.a. 25/06/14
-sub{6}='e06'; % e.u.  01/07/14
-sub{7}='e07'; % a.s.  07/07/14
-sub{8}='e08'; % k.t.  09/07/14
-sub{9}='e09';% d.a.  14/07/14
-sub{10}='e10';% k.l.  15/07/14
-sub{11}='e11';% ab.m.  16/07/14  % from here on, had EOG/EMG
-sub{12}='e12';% b.s.  17/07/14
-sub{13}='e13';% d.t.  21/07/14
-sub{14}='e14';% f.g.  22/07/14
-sub{15}='e15';% r.m.  23/07/14
-sub{16}='e16';% t.p.  24/07/14 % from here on, had attempt Polhemus
-sub{17}='e17';% t.t.  28/07/14
-sub{18}='e18';% k.n.v.  29/07/14
-sub{19}='e19';% j.b.  30/07/14
-sub{20}='e20';% n.m.  31/07/14
-sub{21}='e21';% l.c.  04/08/14
-sub{22}='e22';% a.b.  05/08/14
-sub{23}='e23';% r.c.
-sub{24}='e24';% a.d.
-sub{25}='e25';% j.c.
-sub{26}='e26';% r.s.
-sub{27}='e27';% a.p.
-sub{28}='e28';% w.p.
-sub{29}='e29';% i.r.
-sub{30}='e30';% o.y.l.
-sub{31}='e31';% r.b.
-sub{32}='e32';% i.f.
+sub{5}='e05'; 
+sub{6}='e06'; 
+sub{7}='e07'; 
+sub{8}='e08'; 
+sub{9}='e09';
+sub{10}='e10';
+sub{11}='e11';
+sub{12}='e12';
+sub{13}='e13';
+sub{14}='e14';
+sub{15}='e15';
+sub{16}='e16';
+sub{17}='e17';
+sub{18}='e18';
+sub{19}='e19';
+sub{20}='e20';
+sub{21}='e21';
+sub{22}='e22';
+sub{23}='e23';
+sub{24}='e24';
+sub{25}='e25';
+sub{26}='e26';
+sub{27}='e27';
+sub{28}='e28';
+sub{29}='e29';
+sub{30}='e30';
+sub{31}='e31';
+sub{32}='e32';
 
 if ispc
   rmpath(genpath('D:\matlab\spm8\external\fieldtrip\'))
