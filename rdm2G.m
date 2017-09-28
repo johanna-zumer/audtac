5a3645f2481c49a5a78a62f2ec628989815e55fa
zumerj <zumerj@bham.ac.uk>
2017-09-28 11:21:37 +0100

    new function: convert RDM to G (second moment)

---
+++ rdm2G.m
@@ -0,0 +1,12 @@
+function Gout=rdm2G(rdminput,plotflag,plottext)
+% function Gout=rdm2G(rdminput,plotflag,plottext)
+
+H=eye(size(rdminput))-ones(size(rdminput))/size(rdminput,1); 
+Gout=-0.5*H*rdminput*H;
+
+if plotflag
+  figure;subplot(1,2,1);imagesc(rdminput);colorbar;title(['RDM ' plottext])
+  subplot(1,2,2);imagesc(Gout);colorbar;title(['G ' plottext])
+end
+
+  
