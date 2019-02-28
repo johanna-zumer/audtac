d72fc01bbf783830eda4910c2c308fcd34dba61d
zumerj <zumerj@bham.ac.uk>
2019-02-28 15:48:52 +0000

    new preamble

---
+++ eeg_legomagic_preamble.m
@@ -0,0 +1,93 @@
+clear all
+close all
+if ispc
+  edir='D:\audtac\eeg_data\';
+  esdir='D:\audtac\source_data\';
+  ddir='D:\audtac\legomagic\diaries\';
+  bdir='D:\audtac\behav_data\';
+  sdir='D:\audtac\spss_stuff\';
+  fdir='D:\audtac\figs\';
+  mdir='D:\audtac\structural_MRI\';
+  pdir='D:\audtac\polhemus\';
+else
+  [~,hostname]=system('hostname');
+  if ~isempty(strfind(hostname,'les')) | ~isempty(strfind(hostname,'LES')) % either COLLES-151401 or LES-LINUX_FS3
+    edir='/home/zumerj/audtac/eeg_data/';
+    esdir='/home/zumerj/audtac/source_data/';
+    ddir='/home/zumerj/audtac/legomagic/diaries/';
+    bdir='/home/zumerj/audtac/behav_data/';
+    sdir='/home/zumerj/audtac/spss_stuff/';
+    fdir='/home/zumerj/audtac/figs/';
+    mdir='/home/zumerj/audtac/structural_MRI/';
+    pdir='/home/zumerj/audtac/polhemus/';
+  else % assume on VM linux of psychl-132432
+    edir='/mnt/hgfs/D/audtac/eeg_data/';
+    esdir='/mnt/hgfs/D/audtac/source_data/';
+    ddir='/mnt/hgfs/D/audtac/legomagic/diaries/';
+    bdir='/mnt/hgfs/D/audtac/behav_data/';
+    sdir='/mnt/hgfs/D/audtac/spss_stuff/';
+    fdir='/mnt/hgfs/D/audtac/figs/';
+    mdir='/mnt/hgfs/D/audtac/structural_MRI/';
+    pdir='/mnt/hgfs/D/audtac/polhemus/';
+  end
+end
+cd(edir)
+
+% sub{100}='p01';
+sub{1}='e01';
+sub{2}='e02';
+sub{3}='e03';
+sub{4}='e04';
+%%%  above is pilot, below is real
+sub{5}='e05';
+sub{6}='e06';
+sub{7}='e07';
+sub{8}='e08';
+sub{9}='e09';
+sub{10}='e10';
+sub{11}='e11';
+sub{12}='e12';
+sub{13}='e13';
+sub{14}='e14';
+sub{15}='e15';
+sub{16}='e16';
+sub{17}='e17';
+sub{18}='e18';
+sub{19}='e19';
+sub{20}='e20';
+sub{21}='e21';
+sub{22}='e22';
+sub{23}='e23';
+sub{24}='e24';
+sub{25}='e25';
+sub{26}='e26';
+sub{27}='e27';
+sub{28}='e28';
+sub{29}='e29';
+sub{30}='e30';
+sub{31}='e31';
+sub{32}='e32';
+
+if ispc
+  warning off
+  rmpath(genpath('D:\matlab\spm8\external\fieldtrip\'))
+  rmpath(genpath('D:\fieldtrip_svn\'))
+  rmpath(genpath('D:\fieldtrip_git\'))
+  warning on
+  addpath('D:\fieldtrip_git\')
+else
+  if strfind(hostname,'les')
+  else
+    warning off
+    rmpath(genpath('/mnt/hgfs/D/matlab/spm8/external/fieldtrip/'))
+    rmpath(genpath('/mnt/hgfs/D/fieldtrip_svn/'))
+    rmpath(genpath('/mnt/hgfs/D/fieldtrip_git/'))
+    warning on
+    addpath('/mnt/hgfs/D/fieldtrip_git/')
+  end
+end
+which ft_defaults.m
+ft_defaults;
+load([edir 'iikeep.mat'])
+soades=[-.5 nan -.07 -.02 0 .02 .07 nan .5];
+iiBuse=setdiff(iiBuse,3:7);
