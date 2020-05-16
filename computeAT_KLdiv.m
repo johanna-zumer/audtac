603bfe178605e676cdaecb054a267f8b2daf7653
Johanna Zumer (Psychology) <j.zumer@bham.ac.uk>
2020-05-16 16:33:56 +0100

    new printflag option

--- computeAT_KLdiv.m
+++ computeAT_KLdiv.m
@@ -1,5 +1,8 @@
-function kldiv=computeAT_KLdiv(data1,data2,edges,added)
+function kldiv=computeAT_KLdiv(data1,data2,edges,added,printflag)
 
 [tmp1]=histcounts(data1,edges,'Normalization','probability');tmp1=tmp1+added;
 [tmp2]=histcounts(data2,edges,'Normalization','probability');tmp2=tmp2+added;
+if printflag
+  figure(123);plot([tmp1; tmp2]');
+end
 kldiv=KLDiv(tmp1,tmp2);
